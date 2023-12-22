21c88502419dffb6cf0763710fb4e47ee03d6f5c
mydafzal <momalafzal20@gmail.com>
2023-12-22 21:09:15 +0500

    Update greeting message

--- src/controller.js
+++ src/controller.js
@@ -33,7 +33,7 @@ async function handleTranscription(request, response) {
     // https://ai-backend-five.vercel.app
 
     twiml.play(
-      "https://ai-backend-five.vercel.app/public/greeting-message-michael.mp3"
+      "https://ai-backend-five.vercel.app/public/greeting-message-adam.mp3"
     );
   }
 
