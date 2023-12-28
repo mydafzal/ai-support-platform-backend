75eb6954acf4d362f7620d6fdff1e1b416f23c91
mydafzal <momalafzal20@gmail.com>
2023-12-28 19:11:42 +0500

    Implemented endpoints for google auth access token

---
+++ src/routes/auth.route.js
@@ -0,0 +1,18 @@
+const Router = require("express").Router;
+const { generateOAuthUrl, storeAccessToken } = require("../google-oauth");
+
+const router = Router();
+
+router.get("/oauth-url", (req, res) => {
+  const oauthUrl = generateOAuthUrl();
+  res.status(200).send(oauthUrl);
+});
+
+router.post("/oauth-access-token", async (req, res) => {
+  const { userId, code } = req.body;
+
+  const response = await storeAccessToken(userId, req.body.code);
+  res.status(200).send(response);
+});
+
+module.exports = router;
