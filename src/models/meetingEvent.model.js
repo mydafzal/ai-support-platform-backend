64c27ab0dd08e95b9fd5bc6cc2dc344d9398e7b6
mydafzal <momalafzal20@gmail.com>
2024-01-04 18:44:38 +0500

    Created database model for meeting event

---
+++ src/models/meetingEvent.model.js
@@ -0,0 +1,49 @@
+const { DataTypes } = require("sequelize");
+const { sequelize } = require("../loaders/db");
+const Customer = require("./customer.model");
+
+const MeetingEvent = sequelize.define(
+  "MeetingEvent",
+  {
+    durationInMinutes: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    description: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    availableDays: {
+      type: DataTypes.ARRAY(
+        DataTypes.ENUM(
+          "Monday",
+          "Tuesday",
+          "Wednesday",
+          "Thursday",
+          "Friday",
+          "Saturday",
+          "Sunday"
+        )
+      ),
+      allowNull: false,
+    },
+    availabilityStartTime: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    availabilityEndTime: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+  },
+  {}
+);
+
+MeetingEvent.belongsTo(Customer, { foreignKey: "customerId" });
+// MeetingEvent.sync({ force: true });
+
+module.exports = MeetingEvent;
