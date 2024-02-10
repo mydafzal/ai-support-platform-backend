a8dfcbf2806cafd77d7c1016e5188c186cd78e11
mydafzal <momalafzal20@gmail.com>
2024-02-10 19:32:26 +0500

    Modify functionality for storing business details

--- src/routes/business.route.js
+++ src/routes/business.route.js
@@ -2,58 +2,70 @@ const {
   buyPhoneNumber,
   createVerifyService,
 } = require("../controllers/call.controller");
+
 const Assistant = require("../models/assistant.model");
-const Integration = require("../models/integration.model");
 const Business = require("../models/business.model");
+const User = require("../models/user.model");
+
 const { uploadToS3 } = require("../integrations/s3Storage");
 const { convertTextToSpeech } = require("../integrations/textToSpeech");
-const User = require("../models/user.model");
 
 const router = require("express").Router();
 
 const { v4: uuidv4 } = require("uuid");
+const { z } = require("zod");
+
+const businessDetailsValidationSchema = z.object({
+  userId: z.number(),
+  businessName: z.string(),
+  assistantName: z.string(),
+  voiceId: z.string(),
+  voiceName: z.string(),
+  greetingMessage: z.string(),
+  farewellMessage: z.string(),
+});
 
 router.post("/", async (req, res) => {
   try {
+    const { success, error } =
+      await businessDetailsValidationSchema.safeParseAsync(req.body);
+
+    if (!success) {
+      return res
+        .status(400)
+        .json({ success: false, message: error.errors[0].message });
+    }
+
     const {
-      name,
-      email,
+      userId,
       businessName,
       assistantName,
       voiceName,
       voiceId,
       greetingMessage,
       farewellMessage,
-      externalId,
-      externalType,
     } = req.body;
 
-    let user = await User.findOne({
-      where: {
-        email,
-      },
-    });
+    let user = await User.findByPk(userId);
 
-    if (user?.toJSON()?.email) {
-      return res.status(400).json({ message: "Email already exists." });
+    if (!user?.toJSON()?.email) {
+      return res.status(404).json({
+        succcess: false,
+        message: "User with the given id does not exist.",
+      });
     }
 
     // const twilioNumber = await buyPhoneNumber();
     // const verifyServiceId = await createVerifyService(companyName);
 
-    const business = await Business.create({
+    let business = await Business.create({
       businessName,
       twilioNumber: "+14697074725",
       verifyServiceId: "",
+      userId: user.id,
     });
 
-    user = await User.create({
-      name,
-      email,
-      externalId,
-      externalType,
-      businessId: business.toJSON().id,
-    });
+    business = business.toJSON();
 
     let promises = [
       convertTextToSpeech(greetingMessage, voiceId),
@@ -65,15 +77,15 @@ router.post("/", async (req, res) => {
     );
 
     promises = [
-      uploadToS3(greetingMessageSpeech, business.id, "greetingMessage.mp3"),
-      uploadToS3(farewellMessageSpeech, business.id, "farewellMessage.mp3"),
+      uploadToS3(greetingMessageSpeech, user.id, "greetingMessage.mp3"),
+      uploadToS3(farewellMessageSpeech, user.id, "farewellMessage.mp3"),
     ];
     const [greetingMessageUrl, farewellMessageUrl] = await Promise.all(
       promises
     );
 
     const assistant = await Assistant.create({
-      businessId: business.id,
+      userId: user.id,
       name: assistantName,
       voiceName,
       voiceId,
@@ -82,46 +94,9 @@ router.post("/", async (req, res) => {
       knowledgeBaseName: uuidv4(),
     });
 
-    res.status(201).json({ user, business, assistant });
+    res.status(201).json({ success: true, data: { business, assistant } });
   } catch (error) {
     console.error("Error adding business:", error);
-    res.status(500).json({ error: "Internal Server Error" });
-  }
-});
-
-router.get("/:email", async (req, res) => {
-  try {
-    const { email } = req.params;
-
-    let business = await Business.findOne({
-      where: {
-        email,
-      },
-    });
-
-    const assistant = await Assistant.findOne({
-      where: {
-        customerId: business.id,
-      },
-    });
-
-    const credentials = await Integration.findOne({
-      where: {
-        customerId: business.id,
-      },
-    });
-
-    business = business.toJSON();
-    business.assistant = assistant.toJSON();
-    business.credentials = credentials.toJSON();
-
-    if (!business) {
-      return res.status(404).json({ error: "Customer not found" });
-    }
-
-    res.status(200).json({ response: "" });
-  } catch (error) {
-    console.error("Error fetching customer:", error);
     res.status(500).json({ error: "Internal Server Error" });
   }
 });
