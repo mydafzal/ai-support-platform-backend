25abeeadbb0036d466fc0728ab739112ae30211c
mydafzal <momalafzal20@gmail.com>
2024-03-20 17:10:11 +0500

    feat: Implement functionality for adding team group and assigning group to team members

---
+++ src/models/teamGroup.model.js
@@ -0,0 +1,20 @@
+const { DataTypes } = require("sequelize");
+const { sequelize } = require("../loaders/db");
+const User = require("./user.model");
+
+const TeamGroup = sequelize.define(
+  "TeamGroup",
+  {
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
+  {}
+);
+
+TeamGroup.belongsTo(User, { foreignKey: "userId" });
+
+// TeamGroup.sync({ force: true });
+
+module.exports = TeamGroup;
