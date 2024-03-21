2e8fb3d60b48b005088aed8eefdb3161bc6dec71
mydafzal <momalafzal20@gmail.com>
2024-03-21 11:56:29 +0500

    feat: Implement functionality for deleting a team group

--- src/routes/teamGroup.route.js
+++ src/routes/teamGroup.route.js
@@ -11,6 +11,10 @@ const addTeamGroupValidationSchema = z.object({
   userId: z.number(),
 });
 
+const updateTeamGroupValidationSchema = z.object({
+  name: z.string(),
+});
+
 const assignGroupToMembersValidationSchema = z.object({
   teamMemberIds: z.array(z.number()),
 });
@@ -78,6 +82,54 @@ router.put("/:id/team-members", async (req, res) => {
       .json({ success: true, message: "Updated group of team members." });
   } catch (error) {
     console.error("Error adding teamGroup:", error);
+    res.status(500).json({ success: false, message: "Internal Server Error" });
+  }
+});
+
+router.delete("/:id", async (req, res) => {
+  try {
+    await TeamGroup.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    res.status(204).send();
+  } catch (error) {
+    console.error("Error deleting team group: ", error);
+    res.status(500).json({ success: false, message: "Internal Server Error" });
+  }
+});
+
+router.put("/:id", async (req, res) => {
+  const teamGroupId = req.params.id;
+
+  try {
+    const { success, error } =
+      await updateTeamGroupValidationSchema.safeParseAsync(req.body);
+
+    if (!success) {
+      return res
+        .status(400)
+        .json({ success: false, message: error.errors[0].message });
+    }
+
+    const { name } = req.body;
+
+    await TeamGroup.update(
+      {
+        name,
+      },
+      {
+        where: {
+          id: teamGroupId,
+        },
+      }
+    );
+
+    res.status(200).json({ success: true, message: "Team group updated." });
+  } catch (error) {
+    console.error("Error updating team group: ", error);
     res.status(500).json({ success: false, message: "Internal Server Error" });
   }
 });
