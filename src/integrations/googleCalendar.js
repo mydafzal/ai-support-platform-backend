4c4f74903205cfe2e10ed99d3e793fea5da42922
mydafzal <momalafzal20@gmail.com>
2024-04-17 19:55:05 +0500

    feat: Finalize implementation of meeting schedule functionality
    Here is a breakdown of the changes:
    - Removed un-used and duplicate code
    - Refactored existing implementation
    - Used dynamic access tokens in Calendly and Google Calendar API requests
    - Implement functionality to refreshing expired access tokens and store refreshed token in db

--- src/integrations/googleCalendar.js
+++ src/integrations/googleCalendar.js
@@ -1,45 +1,13 @@
 const { google } = require("googleapis");
-const key = require("../../keyfile.json");
 const credentials = require("../../credentials.json");
-const fs = require("fs");
-const readline = require("readline");
 
-const path = require("path");
-
-const SCOPES = ["https://www.googleapis.com/auth/calendar.events"];
-// const TOKEN_PATH = "./token.json";
-const TOKEN_PATH = path.join(__dirname, "..", "token.json");
+const { BusinessIntegration } = require("../../models");
+const { GOOGLE_CALENDAR_INTEGRATION_ID } = require("../utils/constants");
 
 let auth, calendar;
 
-// async function getCalendarEvents() {
-//   const response = await calendar.events.list({
-//     // calendarId: "primary",
-//     calendarId: "hammadfarooq233@gmail.com",
-//     timeMin: new Date().toISOString(),
-//     maxResults: 10,
-//     singleEvents: true,
-//     orderBy: "startTime",
-//   });
-
-//   const events = response.data.items;
-
-//   console.log("data", response.data);
-
-//   if (events.length) {
-//     console.log("Upcoming events:");
-
-//     events.forEach((event) => {
-//       const start = event.start.dateTime || event.start.date;
-//       console.log(`${start} - ${event.summary}`);
-//     });
-//   } else {
-//     console.log("No upcoming events found.");
-//   }
-// }
-
-async function authorize() {
-  const { client_secret, client_id, redirect_uris } = credentials.installed;
+async function authorize(businessId) {
+  const { client_secret, client_id, redirect_uris } = credentials.web;
 
   const oAuth2Client = new google.auth.OAuth2(
     client_id,
@@ -48,125 +16,89 @@ async function authorize() {
   );
 
   try {
-    const token = fs.readFileSync(TOKEN_PATH);
+    let businessIntegration = await BusinessIntegration.findOne({
+      where: {
+        businessId,
+        integrationId: GOOGLE_CALENDAR_INTEGRATION_ID,
+      },
+    });
 
-    console.log("token path", token);
+    if (!businessIntegration) {
+      return "Can't schedule meeting at this time!";
+    }
 
-    oAuth2Client.setCredentials(JSON.parse(token));
+    businessIntegration = businessIntegration.toJSON();
+    let { accessToken, refreshToken, expirationTime } = businessIntegration;
+
+    console.log("businessIntegration - ", businessIntegration);
+
+    const differenceInMilliseconds =
+      new Date(expirationTime).getTime() - new Date().getTime();
+
+    oAuth2Client.setCredentials({
+      access_token: accessToken,
+      refresh_token: refreshToken,
+      expiry_date: differenceInMilliseconds,
+      token_type: "bearer",
+    });
 
     if (oAuth2Client.isTokenExpiring()) {
-      await refreshAccessToken(oAuth2Client);
+      const newToken = await refreshAccessToken(oAuth2Client);
+
+      oAuth2Client.setCredentials({
+        access_token: newToken.token,
+        refresh_token: oAuth2Client.credentials.refresh_token,
+        token_type: oAuth2Client.credentials.token_type,
+        expiry_date: oAuth2Client.credentials.expiry_date,
+      });
+
+      await BusinessIntegration.update(
+        {
+          accessToken: newToken.token,
+        },
+        {
+          where: {
+            id: businessIntegration.id,
+          },
+        }
+      );
     }
 
     return oAuth2Client;
   } catch (error) {
-    console.log("getAccessToken --------");
-
-    return await getAccessToken(oAuth2Client);
+    console.log("Google Calendar authorize - error - ", error);
   }
-}
-
-async function getAccessToken(oAuth2Client) {
-  const authUrl = oAuth2Client.generateAuthUrl({
-    access_type: "offline",
-    scope: SCOPES,
-  });
-
-  console.log("Authorize this app by visiting this URL:", authUrl);
-
-  const code = await askQuestion("Enter the code from that page here: ");
-
-  try {
-    const token = await getTokenAsync(oAuth2Client, code);
-    await writeTokenToFile(token);
-
-    oAuth2Client.setCredentials(token);
-    return oAuth2Client;
-  } catch (error) {
-    console.error("Error:", error);
-    throw error;
-  }
-}
-
-async function askQuestion(question) {
-  const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout,
-  });
-
-  return new Promise((resolve) => {
-    rl.question(question, (answer) => {
-      rl.close();
-      resolve(answer);
-    });
-  });
-}
-
-async function getTokenAsync(oAuth2Client, code) {
-  return new Promise((resolve, reject) => {
-    oAuth2Client.getToken(code, (err, token) => {
-      if (err) {
-        console.error("Error retrieving access token", err);
-        reject(err);
-      } else {
-        resolve(token);
-      }
-    });
-  });
-}
-
-async function writeTokenToFile(token) {
-  return new Promise((resolve, reject) => {
-    fs.writeFile(TOKEN_PATH, JSON.stringify(token), (err) => {
-      if (err) {
-        reject(err);
-      } else {
-        console.log("Token stored to", TOKEN_PATH);
-        resolve(token);
-      }
-    });
-  });
 }
 
 async function refreshAccessToken(oAuth2Client) {
   try {
     const newToken = await oAuth2Client.getAccessToken();
-
-    oAuth2Client.setCredentials({
-      access_token: newToken.token,
-      refresh_token: oAuth2Client.credentials.refresh_token,
-      scope: oAuth2Client.credentials.scope,
-      token_type: oAuth2Client.credentials.token_type,
-      expiry_date: oAuth2Client.credentials.expiry_date,
-    });
-
-    fs.writeFileSync(TOKEN_PATH, JSON.stringify(oAuth2Client.credentials));
     console.log("Access token refreshed.");
+
+    return newToken;
   } catch (err) {
-    console.error("Error refreshing access token:", err.message);
+    console.error("Error refreshing access token:", err);
   }
 }
 
 async function addEventToGoogleCalendar(
-  callerEmail,
-  eventName,
-  projectType,
+  businessId,
+  customerEmail,
+  meetingDescription,
   start,
   end
 ) {
   if (!auth) {
-    auth = await authorize();
+    auth = await authorize(businessId);
   }
 
   if (!calendar) {
     calendar = google.calendar({ version: "v3", auth });
   }
 
-  console.log("auth", auth);
-
   const event = {
-    summary: eventName,
-    description: `Meeting with Cheetah's customer support for a ${projectType} project.`,
+    summary: "Customer Bot Meeting",
+    description: meetingDescription,
     start: {
       dateTime: start,
       timeZone: "America/Los_Angeles",
@@ -175,8 +107,7 @@ async function addEventToGoogleCalendar(
       dateTime: end,
       timeZone: "America/Los_Angeles",
     },
-    // attendees: [{ email: callerEmail }],
-    attendees: [{ email: callerEmail }],
+    attendees: [{ email: customerEmail }],
     reminders: {
       useDefault: false,
       overrides: [
@@ -196,7 +127,7 @@ async function addEventToGoogleCalendar(
 
   calendar.events.insert(
     {
-      calendarId: "hammadfarooq233@gmail.com",
+      calendarId: "hammad@cheetahagency.com",
       requestBody: event,
       sendNotifications: true,
       sendUpdates: "all",
@@ -211,7 +142,6 @@ async function addEventToGoogleCalendar(
       }
 
       console.log("Event created");
-      // console.log("Event created", event);
     }
   );
 }
