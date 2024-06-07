5a0e633875a68c5d2ee5703d9a1afa351fab2cfc
mydafzal <momalafzal20@gmail.com>
2024-06-07 21:14:16 +0500

    update seeders

--- seeders/20240401105635-add-integrations.js
+++ seeders/20240401105635-add-integrations.js
@@ -19,7 +19,8 @@ module.exports = {
         createdAt: new Date(),
         updatedAt: new Date(),
         recommended: true,
-        authUrl: process.env.HUBSPOT_AUTH_URL,
+        authUrl:
+          "https://app.hubspot.com/oauth/authorize?client_id=c168eab0-d901-49a0-9137-e6cd0212e043&redirect_uri=https://app.customerbot.ai/connect-integration&scope=crm.lists.read%20crm.objects.contacts.read%20crm.objects.contacts.write%20crm.objects.companies.write%20crm.lists.write%20crm.objects.companies.read",
       },
       {
         id: GOOGLE_CALENDAR_INTEGRATION_ID,
@@ -30,7 +31,8 @@ module.exports = {
         createdAt: new Date(),
         updatedAt: new Date(),
         recommended: true,
-        authUrl: process.env.GOOGLE_AUTH_URL,
+        authUrl:
+          "https://accounts.google.com/o/oauth2/v2/auth?access_type=offline&scope=https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fcalendar.events&response_type=code&client_id=466285189832-2hh7mgkft8n2pgc9u5v52r43grfcc1ip.apps.googleusercontent.com&redirect_uri=https%3A%2F%2Fapp.customerbot.ai%2Fconnect-integration",
       },
       {
         id: CALENDLY_INTEGRATION_ID,
@@ -41,7 +43,8 @@ module.exports = {
         createdAt: new Date(),
         updatedAt: new Date(),
         recommended: true,
-        authUrl: process.env.CALENDLY_AUTH_URL,
+        authUrl:
+          "https://calendly.com/oauth/authorize?client_id=8fWfomqyAxBXMtxIJuvQPxGxJ4YKpaWFu8zJnYjmSmo&response_type=code&redirect_uri=https://app.customerbot.ai/connect-integration",
       },
     ]);
   },
