d4f75f22a6420b3f46fc2b9205ff40ce8ae354bd
mydafzal <momalafzal20@gmail.com>
2024-06-13 21:44:12 +0500

    uncomment code for buying phone number

--- src/routes/business.route.js
+++ src/routes/business.route.js
@@ -1,5 +1,5 @@
 const {
-  // buyPhoneNumber,
+  buyPhoneNumber,
   createVerifyService,
 } = require("../controllers/call.controller");
 
@@ -94,13 +94,13 @@ router.post("/", async (req, res) => {
       });
     }
 
-    // const twilioNumber = await buyPhoneNumber();
+    const twilioNumber = await buyPhoneNumber();
     const verifyServiceId = await createVerifyService(businessName);
 
     let business = await Business.create({
       name: businessName,
-      twilioNumber: "+14697074725",
-      // twilioNumber: twilioNumber || "+14697074725",
+      // twilioNumber: "+14697074725",
+      twilioNumber: twilioNumber || "+14697074725",
       verifyServiceId,
       adminUserId: userId,
     });
