d298f3f28f4cf1cba665e47a7245c1694fdcc785
mydafzal <momalafzal20@gmail.com>
2024-07-11 20:53:16 +0500

    feat: Implement functionality for updating subscription based on all possible factors

--- src/routes/subscription.route.js
+++ src/routes/subscription.route.js
@@ -13,11 +13,12 @@ const customizedFeatureSchema = z.object({
 const createSubscriptionValidationSchema = z.object({
   businessId: z.number(),
   planId: z.number(),
-  name: z.string().optional(),
-  password: z
-    .string()
-    .min(4, "Password must contain at least 4 characters.")
-    .optional(),
+  billingCycle: z.enum(["monthly", "yearly"]),
+  customizedFeatures: z.array(customizedFeatureSchema).optional(),
+});
+
+const updateSubscriptionValidationSchema = z.object({
+  newPlanId: z.number().optional(),
   billingCycle: z.enum(["monthly", "yearly"]),
   customizedFeatures: z.array(customizedFeatureSchema).optional(),
 });
@@ -256,6 +257,316 @@ router.post("/", async (req, res) => {
     res
       .status(200)
       .json({ success: true, message: "Subscription created succesfully." });
+  } catch (error) {
+    console.log("create subscription error - ", error);
+    res.status(400).send({ error: { message: error.message } });
+  }
+});
+
+router.put("/:id", async (req, res) => {
+  try {
+    const { success, error } =
+      await updateSubscriptionValidationSchema.safeParseAsync(req.body);
+
+    if (!success) {
+      return res.status(400).json({
+        success: false,
+        message: error.errors[0].message,
+      });
+    }
+
+    const subscriptionId = req.params.id;
+    const { newPlanId, billingCycle, customizedFeatures = [] } = req.body;
+
+    let subscription = await Subscription.findByPk(subscriptionId, {
+      include: [
+        {
+          model: Business,
+          as: "business",
+        },
+      ],
+    });
+
+    if (!subscription) {
+      return res
+        .status(400)
+        .json({ success: false, message: "Invalid subscription id." });
+    }
+
+    subscription = subscription.toJSON();
+
+    const stripeSubscription = await stripe.subscriptions.retrieve(
+      subscription.stripeSubscriptionId
+    );
+
+    const subscriptionItem = stripeSubscription.items.data[0];
+    const currentSubscriptionPrice = subscriptionItem.price;
+
+    const currentBillingCycle =
+      currentSubscriptionPrice.recurring.interval === "month"
+        ? "monthly"
+        : "yearly";
+
+    // Check if the new subscription changes are the same as current subscription details:
+
+    const hasPlanChanged =
+      !newPlanId || (newPlanId && newPlanId === subscription.planId);
+
+    if (
+      hasPlanChanged &&
+      currentBillingCycle === billingCycle &&
+      customizedFeatures.length < 1
+    ) {
+      return res.status(400).json({
+        success: false,
+        message: "No changes were identified in the subscrption.",
+      });
+    }
+
+    let pricingPlan = await PricingPlan.findByPk(
+      newPlanId || subscription.planId
+    );
+
+    if (!pricingPlan) {
+      return res.status(400).json({
+        success: false,
+        message: "Invalid pricing plan id.",
+      });
+    }
+
+    pricingPlan = pricingPlan.toJSON();
+
+    let basePrice = pricingPlan.monthlyBasePrice;
+
+    let totalCost = parseFloat(basePrice);
+
+    if (customizedFeatures.length > 0) {
+      const featureIds = customizedFeatures.map((item) => item.featureId);
+
+      let features = await Feature.findAll({
+        where: {
+          id: {
+            [Op.in]: featureIds,
+          },
+        },
+        include: [
+          {
+            model: PricingPlan,
+            as: "plans",
+            where: {
+              id: newPlanId || subscription.planId,
+            },
+            through: {
+              attributes: [],
+            },
+            attributes: [],
+          },
+        ],
+        attributes: {
+          include: [
+            [Sequelize.col("plans.PlanFeature.baseQuantity"), "baseQuantity"],
+          ],
+          exclude: ["basePlanId"],
+        },
+      });
+
+      features = features.map((item) => item.toJSON());
+
+      let totalExtraCost = 0;
+
+      features.forEach((feature) => {
+        const customQuantity = customizedFeatures.find(
+          (item) => item.featureId == feature.id
+        ).quantity;
+
+        const extraUnits = customQuantity - parseInt(feature.baseQuantity);
+        const featureExtraCost =
+          parseFloat(feature.unitPrice) * parseInt(extraUnits);
+
+        totalExtraCost += featureExtraCost;
+      });
+
+      totalCost += totalExtraCost;
+    }
+
+    if (billingCycle === "yearly") {
+      totalCost = calculateYearlyPrice(
+        totalCost,
+        pricingPlan.yearlyDiscountPercentage
+      );
+    }
+
+    const toalCostInCents = Math.round(totalCost * 100);
+
+    const newSubscriptionPrice = await stripe.prices.create({
+      currency: "usd",
+      unit_amount: toalCostInCents,
+      recurring: {
+        interval: billingCycle === "monthly" ? "month" : "year",
+      },
+      product: pricingPlan.stripeProductId,
+    });
+
+    const paymentMethods = await stripe.customers.listPaymentMethods(
+      subscription.business.stripeCustomerId,
+      {
+        limit: 1,
+      }
+    );
+
+    await stripe.subscriptions.update(subscription.stripeSubscriptionId, {
+      items: [
+        {
+          id: subscriptionItem.id,
+          deleted: true,
+        },
+        {
+          price: newSubscriptionPrice.id,
+        },
+      ],
+      default_payment_method: paymentMethods.data[0].id,
+    });
+
+    if (newPlanId) {
+      await Subscription.update(
+        {
+          planId: pricingPlan.id,
+        },
+        {
+          where: {
+            id: subscription.id,
+          },
+        }
+      );
+    }
+
+    let promises = [];
+
+    // change from pro to enterprise: add companies feature to subscription-feature table
+    // change from enterprise to pro: remove companies features from subscription-feature table
+    // change from pro to enterprise: reset features without customization to base quantity, set custom quantity for customized features.
+
+    // Get all features that are included in the new plan:
+    let newPlanFeatures = await Feature.findAll({
+      include: [
+        {
+          model: PricingPlan,
+          as: "plans",
+          where: {
+            id: newPlanId || subscription.planId,
+          },
+          through: {
+            attributes: [],
+          },
+          attributes: [],
+        },
+      ],
+      attributes: {
+        include: [
+          [Sequelize.col("plans.PlanFeature.baseQuantity"), "baseQuantity"],
+        ],
+        exclude: ["basePlanId"],
+      },
+    });
+
+    newPlanFeatures = newPlanFeatures.map((item) => item.toJSON());
+
+    // Remove features from user's subscription that are not included in the new plan.
+    await SubscriptionFeature.destroy({
+      where: {
+        subscriptionId: subscription.id,
+        featureId: {
+          [Op.notIn]: newPlanFeatures.map((item) => item.id),
+        },
+      },
+    });
+
+    // Add features to user's subscription that are not currently in the subscription but are included in the new plan:
+    const currentSubscriptionFeatures = await SubscriptionFeature.findAll({
+      where: {
+        subscriptionId: subscription.id,
+      },
+      attributes: ["featureId"],
+    });
+
+    const currentSubscriptionFeatureIds = currentSubscriptionFeatures.map(
+      (item) => item.toJSON().featureId
+    );
+
+    let newPlanFeaturesNotInCurrentSubscription = newPlanFeatures.filter(
+      (feature) => !currentSubscriptionFeatureIds.includes(feature.id)
+    );
+
+    newPlanFeaturesNotInCurrentSubscription =
+      newPlanFeaturesNotInCurrentSubscription.map((item) => ({
+        featureId: item.id,
+        subscriptionId: subscription.id,
+        quantity: item.baseQuantity,
+      }));
+
+    await SubscriptionFeature.bulkCreate(
+      newPlanFeaturesNotInCurrentSubscription
+    );
+
+    // Now the subscription features are according to the new plan. Set the quantity of features according to the customizations made to the new plan.
+    if (customizedFeatures.length > 0) {
+      promises = customizedFeatures.map((feature) => {
+        return SubscriptionFeature.update(
+          {
+            quantity: feature.quantity,
+          },
+          {
+            where: {
+              subscriptionId: subscription.id,
+              featureId: feature.featureId,
+            },
+          }
+        );
+      });
+    }
+
+    await Promise.all(promises);
+
+    // Reset the quantity of features that are both in previous and new plan, are not customized in the new plan and were customized in the previous plan...
+
+    res
+      .status(200)
+      .json({ success: true, message: "Subscription updated succesfully." });
+  } catch (error) {
+    console.log("create subscription error - ", error);
+    res.status(400).send({ error: { message: error.message } });
+  }
+});
+
+router.put("/:id/cancel", async (req, res) => {
+  try {
+    let subscription = await Subscription.findByPk(req.params.id);
+
+    if (!subscription) {
+      return res
+        .status(400)
+        .json({ success: false, message: "Invalid subscription id." });
+    }
+
+    subscription = subscription.toJSON();
+
+    const stripeSubscription = await stripe.subscriptions.update(
+      subscription.stripeSubscriptionId,
+      {
+        cancel_at_period_end: true,
+      }
+    );
+
+    const response = {
+      isScheduledForCancellation: stripeSubscription.cancel_at_period_end,
+      currentPeriodEnd: stripeSubscription.current_period_end,
+    };
+
+    res.status(200).json({
+      success: true,
+      message: "Subscription cancelled succesfully.",
+      data: response,
+    });
   } catch (error) {
     console.log("create subscription error - ", error);
     res.status(400).send({ error: { message: error.message } });
