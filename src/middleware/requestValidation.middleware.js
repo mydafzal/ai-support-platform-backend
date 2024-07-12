d9692aa09e646030f62482115398e250691952d8
mydafzal <momalafzal20@gmail.com>
2024-07-12 21:27:23 +0500

    feat: Implement re-usable middleware for validating API request data

---
+++ src/middleware/requestValidation.middleware.js
@@ -0,0 +1,19 @@
+const ResponseHandler = require("../utils/responseHandler");
+
+const validateRequest = (schema) => (req, res, next) => {
+  try {
+    schema.parse(req.body);
+    next();
+  } catch (err) {
+    return ResponseHandler.error(
+      res,
+      400,
+      err.errors.map((error) => ({
+        path: error.path,
+        message: error.message,
+      }))
+    );
+  }
+};
+
+module.exports = validateRequest;
