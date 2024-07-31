9952cee897d9d77e18bad61f50ba04ab868186a8
mydafzal <momalafzal20@gmail.com>
2024-07-31 13:43:15 +0500

    fix: fix bugs in validation middleware

--- src/middleware/requestValidation.middleware.js
+++ src/middleware/requestValidation.middleware.js
@@ -2,17 +2,16 @@ const ResponseHandler = require("../utils/responseHandler");
 
 const validateRequest = (schema) => (req, res, next) => {
   try {
-    schema.parse({ ...req.body, files: req.files });
+    schema.safeParse(req.body);
     next();
   } catch (err) {
-    return ResponseHandler.error(
-      res,
-      400,
-      err.errors.map((error) => ({
+    return ResponseHandler.error(res, {
+      statusCode: 400,
+      message: err.errors.map((error) => ({
         path: error.path,
         message: error.message,
-      }))
-    );
+      })),
+    });
   }
 };
 
