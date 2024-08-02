065bbe5633956ec2614957e0d7fb60506419716f
mydafzal <momalafzal20@gmail.com>
2024-08-02 20:34:52 +0500

    refactor: update business model for lead mode option

--- models/business.js
+++ models/business.js
@@ -77,6 +77,7 @@ module.exports = (sequelize, DataTypes) => {
       twilioNumber: { type: DataTypes.STRING, allowNull: false },
       verifyServiceId: DataTypes.STRING,
       stripeCustomerId: DataTypes.STRING,
+      leadMode: DataTypes.BOOLEAN,
     },
     {
       sequelize,
