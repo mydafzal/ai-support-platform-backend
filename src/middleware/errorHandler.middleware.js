da378940091ac367fa0b21440e363608e428441a
mydafzal <momalafzal20@gmail.com>
2024-08-08 20:05:46 +0500

    feat: add error log in global error handler

--- src/middleware/errorHandler.middleware.js
+++ src/middleware/errorHandler.middleware.js
@@ -1,12 +1,30 @@
 const ResponseHandler = require("../utils/responseHandler");
 
 const errorHandler = (err, req, res, next) => {
+  const { statusCode = 500, message = "Internal Server Error", stack } = err;
+
+  const logMessage = `
+   Error occurred:
+   Status Code: ${statusCode}
+   Message: ${message}
+   Stack Trace: ${stack || "No stack trace available"}
+   
+   Request Details:
+   Method: ${req.method}
+   URL: ${req.originalUrl}
+   Body: ${JSON.stringify(req.body, null, 2)}
+   Headers: ${JSON.stringify(req.headers, null, 2)}
+ `;
+
   if (!res.headersSent) {
     ResponseHandler.error(res, {
       statusCode: err.statusCode || 500,
       message: err.message || "Internal Server Error",
     });
+
+    console.error(logMessage);
   } else {
+    console.error(logMessage);
     next(err);
   }
 };
