218e772f065382593c36279b541284775f9e9558
mydafzal <momalafzal20@gmail.com>
2024-08-08 20:06:49 +0500

    refactor: refactor all APIs of auth module and move logic to auth server

--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -1,431 +1,89 @@
 const Router = require("express").Router;
 const router = Router();
 
-const bcrypt = require("bcrypt");
-const jwt = require("jsonwebtoken");
+const AuthService = require("../services/auth.service");
 
-const { User, Business, Assistant, Invitation } = require("../../models");
-
-const { z } = require("zod");
-const { sendEmail } = require("../integrations/nodemailer");
+const validateRequest = require("../middleware/requestValidation.middleware");
+const ResponseHandler = require("../utils/responseHandler");
 
 const {
-  generateEmailVerificationToken,
-  generateEmailLink,
-  generateJWT,
-} = require("../utils/helpers");
+  loginSchema,
+  emailSchema,
+  passwordSchema,
+} = require("../validators/auth.validator");
 
-const loginValidationSchema = z.object({
-  email: z.string().email(),
-  name: z.string().optional(),
-  password: z
-    .string()
-    .min(4, "Password must contain at least 4 characters.")
-    .optional(),
-  externalType: z.enum(["Google", "Apple", ""]),
-  profileImageUrl: z.string().optional(),
+router.post("/login", validateRequest(loginSchema), async (req, res, next) => {
+  try {
+    const result = await AuthService.login(req.body);
+    ResponseHandler.success(res, { data: result });
+  } catch (error) {
+    console.error("Error during login - ", error);
+    next(error);
+  }
 });
 
-const emailValidationSchema = z.string().email();
-const passwordValidationSchema = z.string().min(4);
-
-router.post("/login", async (req, res) => {
-  try {
-    const { success, error } = await loginValidationSchema.safeParseAsync(
-      req.body
-    );
-
-    if (!success) {
-      return res.status(400).json({
-        success: false,
-        message: error.errors[0].message,
+router.post(
+  "/forgot-password",
+  validateRequest(emailSchema),
+  async (req, res, next) => {
+    try {
+      await AuthService.generatePasswordResetLink(req.body);
+      ResponseHandler.success(res, {
+        message: "Password reset instructions sent to your email.",
       });
+    } catch (error) {
+      console.log("Error forgot-password", error);
+      next(error);
     }
-
-    const { email, password, externalType, name, profileImageUrl } = req.body;
-
-    let user = await User.findOne({
-      where: { email },
-      attributes: {
-        exclude: ["emailVerificationToken", "resetPasswordToken"],
-      },
-    });
-
-    let invitation = await Invitation.findOne({
-      where: {
-        email,
-      },
-    });
-    invitation = invitation?.toJSON();
-
-    // Social authentication
-    if (externalType === "Google" || externalType === "Apple") {
-      console.log("externalType - ", externalType);
-      console.log("user - ", user);
-
-      if (!user) {
-        if (!name) {
-          return res.status(400).json({
-            success: false,
-            message: "Name is required.",
-          });
-        }
-
-        user = await User.create({
-          name,
-          email,
-          externalType,
-          emailVerified: true,
-          profileImageUrl,
-          businessId: invitation ? invitation.businessId : null,
-          role: invitation ? "TeamMember" : null,
-        });
-
-        if (invitation && invitation?.status !== "Accepted") {
-          await Invitation.update(
-            {
-              status: "Accepted",
-            },
-            {
-              where: {
-                email,
-              },
-            }
-          );
-
-          invitation.status = "Accepted";
-        }
-      } else if (!user.toJSON().externalType) {
-        return res.status(400).json({
-          success: false,
-          message:
-            "This is not the authentication method you used during signup.",
-        });
-      }
-
-      user = user.toJSON();
-    }
-
-    // Perform email/password authentication
-    else {
-      if (!user || !password || !user.password) {
-        return res
-          .status(401)
-          .json({ success: false, message: "Invalid email and/or password." });
-      }
-
-      user = user.toJSON();
-
-      const result = await bcrypt.compare(password, user.password);
-      if (!result) {
-        return res
-          .status(401)
-          .json({ success: false, message: "Invalid email and/or password" });
-      }
-
-      if (!user?.emailVerified) {
-        const emailVerificationToken = generateEmailVerificationToken(user.id);
-
-        const emailLink = generateEmailLink(
-          req,
-          "email-verification",
-          `token=${emailVerificationToken}`
-        );
-
-        const emailTemplate = `Please verify your email by clicking <a href="${emailLink}">here</a>`;
-
-        await sendEmail(user?.email, emailTemplate);
-
-        await User.update(
-          {
-            emailVerificationToken,
-          },
-          {
-            where: {
-              id: user.id,
-            },
-          }
-        );
-
-        const payload = {
-          ...user,
-        };
-        const token = generateJWT(payload);
-
-        return res.status(200).json({
-          success: success,
-          data: token,
-          message:
-            "Email not verified. A new email verification link has been sent.",
-        });
-      }
-    }
-
-    delete user.password;
-
-    let business = await Business.findOne({
-      where: {
-        id: user.businessId,
-      },
-      include: [
-        {
-          model: Assistant,
-          as: "assistant",
-        },
-      ],
-    });
-
-    business = business?.toJSON();
-    user.business = business;
-
-    const payload = {
-      ...user,
-      invitation,
-    };
-
-    const token = generateJWT(payload);
-    return res.status(200).json({ success: true, data: token });
-  } catch (error) {
-    console.error("Error during login:", error);
-    res.status(500).json({ success: false, message: "Internal Server Error." });
   }
-});
+);
 
-router.post("/forgot-password", async (req, res) => {
-  const { email } = req.body;
+router.post(
+  "/reset-password/:token",
+  validateRequest(passwordSchema),
+  async (req, res, next) => {
+    const { token } = req.params;
+    const { password } = req.body;
 
-  try {
-    const { success, error } = await emailValidationSchema.safeParseAsync(
-      email
-    );
-    if (!success) {
-      return res.status(400).json({ success: false, message: error.message });
-    }
-
-    let user = await User.findOne({
-      where: {
-        email,
-      },
-    });
-
-    if (!user) {
-      return res.status(404).json({ success: false, message: "Invalid email" });
-    } else if (user.toJSON().externalType) {
-      return res.status(404).json({
-        success: false,
-        message:
-          "Password cannot be reset for the authentication method you used during signup.",
+    try {
+      await AuthService.resetPassword({ token, password });
+      ResponseHandler.success(res, {
+        message: "Password reset successfully.",
       });
+    } catch (error) {
+      console.error("Error occurred while resetting passsword: ", error);
+      next(error);
     }
-
-    const token = jwt.sign({ userId: user.id }, process.env.JWT_SECRET, {
-      expiresIn: "1h",
-    });
-
-    user.resetPasswordToken = token;
-    await user.save();
-
-    const emailLink = generateEmailLink(
-      req,
-      "setup-password",
-      `token=${token}`
-    );
-    const emailTemplate = `Reset your password by clicking <a href="${emailLink}">here</a>`;
-    await sendEmail(email, emailTemplate);
-
-    res.status(200).json({
-      success: true,
-      message: "Password reset instructions sent to your email.",
-    });
-  } catch (error) {
-    console.log("Error forgot-password", error);
-    res.status(500).json({
-      success: false,
-      message: "Internal server error.",
-    });
   }
-});
+);
 
-router.post("/reset-password/:token", async (req, res) => {
-  const { token } = req.params;
-  const { password } = req.body;
-
-  try {
-    const { success, error } = await passwordValidationSchema.safeParseAsync(
-      password
-    );
-
-    if (!success) {
-      return res.status(400).json({ success: false, message: error.message });
-    }
-
-    let user = await User.findOne({ where: { resetPasswordToken: token } });
-
-    if (!user) {
-      return res
-        .status(404)
-        .json({ success: false, message: "Invalid or expired token." });
-    }
-
-    const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
-    if (!decodedToken || decodedToken.userId !== user.toJSON().id) {
-      return res
-        .status(404)
-        .json({ success: false, message: "Invalid or expired token." });
-    }
-
-    const hashedPassword = await bcrypt.hash(password, 10);
-    user.password = hashedPassword;
-    user.resetPasswordToken = null;
-    await user.save();
-
-    res
-      .status(200)
-      .json({ success: true, message: "Password reset successfully." });
-  } catch (error) {
-    console.error("Error occurred while resetting passsword: ", error);
-    res.status(500).json({ success: false, message: "Internal server error." });
-  }
-});
-
-router.get("/verify-email", async (req, res) => {
+router.get("/verify-email", async (req, res, next) => {
   try {
     const { token } = req.query;
+    const authToken = await AuthService.verifyEmail({ token });
 
-    if (!token) {
-      return res
-        .status(400)
-        .json({ success: false, message: "Please provide a valid token" });
-    }
-
-    const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
-
-    let user = await User.findByPk(decodedToken.userId, {
-      attributes: {
-        exclude: ["resetPasswordToken", "password"],
-      },
-    });
-
-    if (!user?.toJSON() || user.toJSON().emailVerificationToken !== token) {
-      return res.status(400).json({ message: "Invalid or expired token" });
-    }
-
-    let invitation = await Invitation.findOne({
-      email: user.toJSON().email,
-    });
-
-    if (invitation) {
-      invitation = invitation.toJSON();
-
-      await Invitation.update(
-        {
-          status: "Accepted",
-        },
-        {
-          where: {
-            id: invitation.id,
-          },
-        }
-      );
-
-      user.role = "TeamMember";
-      user.businessId = invitation.businessId;
-    }
-
-    user.emailVerified = true;
-    user.emailVerificationToken = null;
-
-    await user.save();
-
-    user = user.toJSON();
-    delete user.emailVerificationToken;
-
-    let business = await Business.findOne({
-      where: {
-        id: user.businessId,
-      },
-      include: [
-        {
-          model: Assistant,
-          as: "assistant",
-        },
-      ],
-    });
-
-    business = business?.toJSON();
-    user.business = business;
-
-    const payload = {
-      ...user,
-      invitation,
-    };
-
-    const authToken = generateJWT(payload);
-
-    res.status(200).json({
-      success: true,
+    ResponseHandler.success(res, {
       data: authToken,
       message: "Email verified successfully",
     });
-  } catch (err) {
+  } catch (error) {
     console.error("Error verifying email:", err);
-    res.status(500).json({ message: "Internal server error" });
+    next(error);
   }
 });
 
-router.post("/resend-verification-email", async (req, res) => {
-  const { email, redirectUri } = req.body;
-
-  try {
-    const { success, error } = await emailValidationSchema.safeParseAsync(
-      email
-    );
-
-    if (!success) {
-      return res.status(400).json({ success: false, message: error.message });
+router.post(
+  "/resend-verification-email",
+  validateRequest(emailSchema),
+  async (req, res) => {
+    try {
+      await AuthService.resendEmailVerificationLink(req.body);
+      ResponseHandler.success(res, { message: "Verification email resent" });
+    } catch (err) {
+      console.error("Error sending email verification link - ", err);
+      res.status(500).json({ message: "Internal server error" });
     }
-
-    let user = await User.findOne({
-      where: {
-        email,
-      },
-    });
-
-    if (!user) {
-      return res
-        .status(400)
-        .json({ success: false, message: "User does not exist." });
-    }
-
-    if (user.toJSON().emailVerified) {
-      return res
-        .status(400)
-        .json({ success: false, message: "User is already verified." });
-    }
-
-    const emailVerificationToken = generateEmailVerificationToken(
-      user.toJSON().id
-    );
-
-    user.emailVerificationToken = emailVerificationToken;
-    await user.save();
-
-    const emailLink = generateEmailLink(
-      req,
-      "email-verification",
-      `token=${emailVerificationToken}&redirectUri=${redirectUri}`
-    );
-
-    const emailTemplate = `Please verify your email by clicking <a href=${emailLink}>here</a>`;
-    await sendEmail(email, emailTemplate);
-
-    res
-      .status(200)
-      .json({ success: true, message: "Verification email resent" });
-  } catch (err) {
-    console.error("Error verifying email:", err);
-    res.status(500).json({ message: "Internal server error" });
   }
-});
+);
 
 module.exports = router;
---
+++ src/services/auth.service.js
@@ -0,0 +1,341 @@
+const { User, Business, Assistant, Invitation } = require("../../models");
+const { sendEmail } = require("../integrations/nodemailer");
+const {
+  generateEmailVerificationToken,
+  generateEmailLink,
+} = require("../utils/helpers");
+
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+
+async function login(data) {
+  const { email, password, externalType, name, profileImageUrl } = data;
+
+  let user = await User.findOne({
+    where: { email },
+    attributes: {
+      exclude: ["emailVerificationToken", "resetPasswordToken"],
+    },
+    raw: true,
+  });
+
+  let invitation = await Invitation.findOne({
+    where: {
+      email,
+    },
+    raw: true,
+  });
+
+  if (externalType === "Google" || externalType === "Apple") {
+    if (!user) {
+      if (!name) {
+        throw { statusCode: 400, message: "Name is required." };
+      }
+
+      user = await User.create({
+        name,
+        email,
+        externalType,
+        emailVerified: true,
+        profileImageUrl,
+        businessId: invitation ? invitation.businessId : null,
+        role: invitation ? "TeamMember" : null,
+      });
+
+      if (invitation && invitation?.status !== "Accepted") {
+        await Invitation.update(
+          {
+            status: "Accepted",
+          },
+          {
+            where: {
+              email,
+            },
+          }
+        );
+
+        invitation.status = "Accepted";
+      }
+    } else if (!user.externalType) {
+      throw {
+        statusCode: 401,
+        message:
+          "This is not the authentication method you used during signup.",
+      };
+    }
+  }
+
+  // Email/password authentication
+  else {
+    if (!user || !password || !user.password) {
+      throw { statusCode: 401, message: "Invalid email and/or password." };
+    }
+
+    const result = await bcrypt.compare(password, user.password);
+    if (!result) {
+      throw { statusCode: 401, message: "Invalid email and/or password" };
+    }
+
+    if (!user?.emailVerified) {
+      const emailVerificationToken = generateEmailVerificationToken(user.id);
+
+      const emailLink = generateEmailLink(
+        "email-verification",
+        `token=${emailVerificationToken}`
+      );
+
+      const emailTemplate = `Please verify your email by clicking <a href="${emailLink}">here</a>`;
+
+      await sendEmail(user?.email, emailTemplate);
+
+      await User.update(
+        {
+          emailVerificationToken,
+        },
+        {
+          where: {
+            id: user.id,
+          },
+        }
+      );
+
+      const token = generateJWT({ ...user });
+
+      return {
+        data: token,
+        message:
+          "Email not verified. A new email verification link has been sent.",
+      };
+    }
+  }
+
+  delete user.password;
+
+  let business = await Business.findOne({
+    where: {
+      id: user.businessId,
+    },
+    include: [
+      {
+        model: Assistant,
+        as: "assistant",
+      },
+    ],
+    raw: true,
+    nest: true,
+  });
+
+  user.business = business;
+
+  const payload = {
+    ...user,
+    invitation,
+  };
+
+  return generateJWT(payload);
+}
+
+async function generatePasswordResetLink(data) {
+  const { email } = data;
+
+  let user = await User.findOne({
+    where: {
+      email,
+    },
+    raw: true,
+  });
+
+  if (!user) {
+    throw {
+      statusCode: 401,
+      message: "Invalid email",
+    };
+  } else if (user.externalType) {
+    throw {
+      statusCode: 401,
+      message:
+        "Password cannot be reset for the authentication method you used during signup.",
+    };
+  }
+
+  const token = jwt.sign({ userId: user.id }, process.env.JWT_SECRET, {
+    expiresIn: "1h",
+  });
+
+  await User.update(
+    {
+      resetPasswordToken: token,
+    },
+    {
+      where: {
+        email,
+      },
+    }
+  );
+
+  const emailLink = generateEmailLink("setup-password", `token=${token}`);
+  const emailTemplate = `Reset your password by clicking <a href="${emailLink}">here</a>`;
+  await sendEmail(email, emailTemplate);
+}
+
+async function resetPassword(data) {
+  const { token } = data;
+
+  let user = await User.findOne({
+    where: { resetPasswordToken: token },
+    raw: true,
+  });
+
+  if (!user) {
+    throw { statusCode: 400, message: "Invalid or expired token." };
+  }
+
+  const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
+
+  if (!decodedToken || decodedToken.userId !== user.id) {
+    throw { statusCode: 400, message: "Invalid or expired token." };
+  }
+
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  await User.update(
+    {
+      password: hashedPassword,
+      resetPasswordToken: null,
+    },
+    {
+      where: {
+        id: user.id,
+      },
+    }
+  );
+}
+
+async function verifyEmail(data) {
+  const { token } = data;
+
+  if (!token) {
+    throw { statusCode: 400, message: "Invalid or expired token." };
+  }
+
+  const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
+
+  let user = await User.findByPk(decodedToken.userId, {
+    attributes: {
+      exclude: ["resetPasswordToken", "password"],
+    },
+    raw: true,
+  });
+
+  if (!user || user.emailVerificationToken !== token) {
+    throw { statusCode: 400, message: "Invalid or expired token." };
+  }
+
+  let invitation = await Invitation.findOne({
+    email: user.email,
+    raw: true,
+  });
+
+  if (invitation) {
+    await Invitation.update(
+      {
+        status: "Accepted",
+      },
+      {
+        where: {
+          id: invitation.id,
+        },
+      }
+    );
+
+    user.role = "TeamMember";
+    user.businessId = invitation.businessId;
+  }
+
+  await User.update(
+    {
+      emailVerified: true,
+      emailVerificationToken: null,
+    },
+    {
+      where: {
+        id: user.id,
+      },
+    }
+  );
+
+  delete user.emailVerificationToken;
+
+  let business = await Business.findOne({
+    where: {
+      id: user.businessId,
+    },
+    include: [
+      {
+        model: Assistant,
+        as: "assistant",
+      },
+    ],
+    raw: true,
+    nest: true,
+  });
+
+  user.business = business;
+
+  const payload = {
+    ...user,
+    invitation,
+  };
+
+  return generateJWT(payload);
+}
+
+async function resendEmailVerificationLink(data) {
+  const { email, redirectUri } = data;
+
+  let user = await User.findOne({
+    where: {
+      email,
+    },
+    raw: true,
+  });
+
+  if (!user) {
+    throw { statusCode: 400, message: "Invalid email" };
+  }
+
+  if (user.emailVerified) {
+    throw { statusCode: 400, message: "User is already verified." };
+  }
+
+  const emailVerificationToken = generateEmailVerificationToken(user.id);
+
+  await User.update(
+    {
+      emailVerificationToken,
+    },
+    {
+      where: {
+        id: user.id,
+      },
+    }
+  );
+
+  const emailLink = generateEmailLink(
+    "email-verification",
+    `token=${emailVerificationToken}&redirectUri=${redirectUri}`
+  );
+
+  const emailTemplate = `Please verify your email by clicking <a href=${emailLink}>here</a>`;
+  await sendEmail(email, emailTemplate);
+}
+
+const AuthService = {
+  login,
+  generatePasswordResetLink,
+  resetPassword,
+  verifyEmail,
+  resendEmailVerificationLink,
+};
+
+module.exports = AuthService;
