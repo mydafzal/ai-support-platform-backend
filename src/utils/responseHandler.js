cb879381ca98e5a1d9c524f4947c21acc3c2e7c8
mydafzal <momalafzal20@gmail.com>
2024-08-08 20:11:15 +0500

    refactor: add pagination details to API response, for paginated APIs

--- src/utils/responseHandler.js
+++ src/utils/responseHandler.js
@@ -1,6 +1,8 @@
 class ResponseHandler {
-  static success(res, { statusCode = 200, data, message }) {
-    return res.status(statusCode).json({ success: true, data, message });
+  static success(res, { statusCode = 200, data, message, pagination }) {
+    return res
+      .status(statusCode)
+      .json({ success: true, data, message, pagination });
   }
 
   static error(res, { statusCode = 500, message }) {
