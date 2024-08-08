86a92e0fcc81d8ac3682bce32350460af59d79a8
mydafzal <momalafzal20@gmail.com>
2024-08-08 20:12:19 +0500

    refactor: refactor request validators of auth module

---
+++ src/validators/auth.validator.js
@@ -0,0 +1,24 @@
+const { z } = require("zod");
+
+const loginSchema = z.object({
+  email: z.string().email(),
+  name: z.string().optional(),
+  password: z
+    .string()
+    .min(4, "Password must contain at least 4 characters.")
+    .optional(),
+  externalType: z.enum(["Google", "Apple", ""]),
+  profileImageUrl: z.string().optional(),
+});
+
+const emailSchema = z.object({
+  email: z.string().email(),
+});
+
+const passwordSchema = z.string().min(4);
+
+module.exports = {
+  loginSchema,
+  emailSchema,
+  passwordSchema,
+};
