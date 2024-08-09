224a75249761ccc31b67d0ddbfeaa0c380777543
mydafzal <momalafzal20@gmail.com>
2024-08-09 20:03:44 +0500

    fix: fix bugs in signup and verify email functionality

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -1,15 +1,31 @@
-const { User, Invitation, Business } = require("../../models");
+const fs = require("fs");
+const path = require("path");
 
-const bcrypt = require("bcrypt");
+const {
+  User,
+  Invitation,
+  Business,
+  BusinessMembership,
+} = require("../../models");
+
 const {
   generateEmailVerificationToken,
   generateEmailLink,
   generateJWT,
 } = require("../utils/helpers");
 const { sendEmail } = require("../integrations/nodemailer");
+
 const SubscriptionService = require("./subscription.service");
-const { TEAM_MEMBERS_FEATURE_ID } = require("../utils/constants");
+const {
+  TEAM_MEMBERS_FEATURE_ID,
+  STORAGE_BASE_PATH,
+  PROFILE_IMAGES_BASE_URL,
+} = require("../utils/constants");
+
+const bcrypt = require("bcrypt");
 const saltRounds = 10;
+
+const sequelize = require("sequelize");
 
 async function registerUser(data) {
   const { name, email, password } = data;
@@ -24,13 +40,6 @@ async function registerUser(data) {
   if (user) {
     throw { statusCode: 400, message: "Email already exists." };
   }
-
-  let invitation = await Invitation.findOne({
-    where: {
-      email,
-    },
-    raw: true,
-  });
 
   let hashedPassword = await bcrypt.hash(password, saltRounds);
 
@@ -71,8 +80,15 @@ async function registerUser(data) {
   const emailTemplate = `Please verify your email by clicking <a href="${emailLink}">here</a>`;
   await sendEmail(user.email, emailTemplate);
 
-  const payload = { ...user, invitation };
-  return generateJWT(payload);
+  const businessMemberships = await BusinessMembership.findAll({
+    where: {
+      userId: user.id,
+    },
+    attributes: ["businessId"],
+    raw: true,
+  });
+
+  return generateJWT({ ...user, businessMemberships });
 }
 
 async function checkEmailAvailability(data) {
@@ -88,11 +104,11 @@ async function checkEmailAvailability(data) {
 }
 
 async function updateUser(data) {
-  const { userId, name, email, phone, teamGroupId, businessId } = data;
+  const { userId, name, email, phone, file } = data;
 
   let user = await User.findByPk(userId, {
     attributes: {
-      exclude: ["emailVerificationToken", "resetPasswordToken"],
+      exclude: ["password", "emailVerificationToken", "resetPasswordToken"],
     },
     raw: true,
   });
@@ -101,52 +117,52 @@ async function updateUser(data) {
     throw { statusCode: 404, message: "Invalid user id." };
   }
 
-  if (businessId || businessId == "") {
-    const currentBusinessId = user.businessId;
+  // if (businessId || businessId == "") {
+  //   const currentBusinessId = user.businessId;
 
-    user.businessId = businessId === "" ? null : businessId;
+  //   user.businessId = businessId === "" ? null : businessId;
 
-    // IF the user is to removed from the organization, then also delete the user's invitation and trigger email to the removed user.
-    if (businessId === "") {
-      const invitation = await Invitation.findOne({
-        where: {
-          email: user.email,
-        },
-        raw: true,
-      });
+  //   // IF the user is to removed from the organization, then also delete the user's invitation and trigger email to the removed user.
+  //   if (businessId === "") {
+  //     const invitation = await Invitation.findOne({
+  //       where: {
+  //         email: user.email,
+  //       },
+  //       raw: true,
+  //     });
 
-      if (invitation) {
-        await Invitation.destroy({
-          where: {
-            email: user.email,
-          },
-        });
+  //     if (invitation) {
+  //       await Invitation.destroy({
+  //         where: {
+  //           email: user.email,
+  //         },
+  //       });
 
-        const business = await Business.findOne({
-          where: {
-            id: currentBusinessId,
-          },
-          raw: true,
-        });
+  //       const business = await Business.findOne({
+  //         where: {
+  //           id: currentBusinessId,
+  //         },
+  //         raw: true,
+  //       });
 
-        let adminUser = await User.findByPk(business.adminUserId, {
-          raw: true,
-        });
+  //       let adminUser = await User.findByPk(business.adminUserId, {
+  //         raw: true,
+  //       });
 
-        if (adminUser) {
-          let emailTemplate;
+  //       if (adminUser) {
+  //         let emailTemplate;
 
-          if (invitation.status === "Pending") {
-            emailTemplate = `Your invitation for organization ${name} has been cancelled.`;
-          } else {
-            emailTemplate = `${adminUser.email} removed you from organization ${name}.`;
-          }
+  //         if (invitation.status === "Pending") {
+  //           emailTemplate = `Your invitation for organization ${name} has been cancelled.`;
+  //         } else {
+  //           emailTemplate = `${adminUser.email} removed you from organization ${name}.`;
+  //         }
 
-          await sendEmail(invitation.email, emailTemplate);
-        }
-      }
-    }
-  }
+  //         await sendEmail(invitation.email, emailTemplate);
+  //       }
+  //     }
+  //   }
+  // }
 
   if (phone != undefined) {
     user.phone = phone;
@@ -157,22 +173,22 @@ async function updateUser(data) {
     user.name = name;
   }
 
-  if (teamGroupId || teamGroupId == "") {
-    user.teamGroupId = teamGroupId === "" ? null : teamGroupId;
+  // if (teamGroupId || teamGroupId == "") {
+  //   user.teamGroupId = teamGroupId === "" ? null : teamGroupId;
 
-    await Invitation.update(
-      {
-        teamGroupId: teamGroupId === "" ? null : teamGroupId,
-      },
-      {
-        where: {
-          email: user.email,
-        },
-      }
-    );
-  }
+  //   await Invitation.update(
+  //     {
+  //       teamGroupId: teamGroupId === "" ? null : teamGroupId,
+  //     },
+  //     {
+  //       where: {
+  //         email: user.email,
+  //       },
+  //     }
+  //   );
+  // }
 
-  if (req.file) {
+  if (file) {
     let profileImageUrl;
 
     if (user.profileImageUrl) {
@@ -188,7 +204,7 @@ async function updateUser(data) {
       const newFilePath = path.join(
         STORAGE_BASE_PATH,
         `profile-images`,
-        req.file.filename
+        file.filename
       );
 
       if (fs.existsSync(existingFilePath)) {
@@ -199,7 +215,7 @@ async function updateUser(data) {
 
       profileImageUrl = `${PROFILE_IMAGES_BASE_URL}/${filename}`;
     } else {
-      profileImageUrl = `${PROFILE_IMAGES_BASE_URL}/${req.file.filename}`;
+      profileImageUrl = `${PROFILE_IMAGES_BASE_URL}/${file.filename}`;
     }
 
     user.profileImageUrl = profileImageUrl;
@@ -209,16 +225,16 @@ async function updateUser(data) {
     user.email = email;
     user.emailVerified = false;
 
-    await Invitation.update(
-      {
-        email,
-      },
-      {
-        where: {
-          email,
-        },
-      }
-    );
+    // await Invitation.update(
+    //   {
+    //     email,
+    //   },
+    //   {
+    //     where: {
+    //       email,
+    //     },
+    //   }
+    // );
   }
 
   await User.update(
@@ -231,9 +247,6 @@ async function updateUser(data) {
       },
     }
   );
-
-  delete user.password;
-  delete user.emailVerificationToken;
 
   return {
     name: user.name,
@@ -285,10 +298,54 @@ async function removeUserFromBusiness(data) {
   }
 }
 
+async function getUsersByBusiness(data) {
+  const { businessId } = data;
+
+  let users = await User.findAll({
+    where: {
+      businessId,
+    },
+    attributes: {
+      exclude: [
+        "password",
+        "emailVerificationToken",
+        "resetPasswordToken",
+        "externalType",
+      ],
+    },
+    include: [{ model: Group, as: "group", attributes: ["name", "id"] }],
+    raw: true,
+    nest: true,
+  });
+
+  let whereCondition = {
+    businessId,
+  };
+
+  if (users?.length > 0) {
+    whereCondition.email = {
+      [sequelize.Op.notIn]: users.map((item) => item.email),
+    };
+  }
+
+  let invitations = await Invitation.findAll({
+    where: whereCondition,
+    attributes: {
+      exclude: ["token"],
+    },
+    include: [{ model: Group, as: "group", attributes: ["name", "id"] }],
+    raw: true,
+    nest: true,
+  });
+
+  return [...users, ...invitations];
+}
+
 const UserService = {
   registerUser,
   updateUser,
   checkEmailAvailability,
   removeUserFromBusiness,
+  getUsersByBusiness,
 };
 module.exports = UserService;
