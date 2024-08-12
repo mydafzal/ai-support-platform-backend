ede5ea00ada0b3ff4013afbe4d073e0fea4c5efe
mydafzal <momalafzal20@gmail.com>
2024-08-12 18:31:35 +0500

    refactor: implement functionality for retrieving a speicfic group

--- src/services/group.service.js
+++ src/services/group.service.js
@@ -63,5 +63,66 @@ async function getGroupsByBusiness(data) {
   });
 }
 
-const GroupService = { getGroupsByBusiness };
+async function getGroupById(data) {
+  const { groupId } = data;
+
+  // Subqueries for counts
+  const chatCountSubquery = sequelize.literal(`
+  (SELECT COUNT(*)
+   FROM "ChatGroupAssignments" AS "ChatGroupAssignments"
+   WHERE "ChatGroupAssignments"."groupId" = "Group"."id")
+`);
+
+  const userCountSubquery = sequelize.literal(`
+  (SELECT COUNT(*)
+   FROM "GroupMemberships" AS "GroupMemberships"
+   WHERE "GroupMemberships"."groupId" = "Group"."id")
+`);
+
+  const invitationCountSubquery = sequelize.literal(`
+  (SELECT COUNT(*)
+   FROM "Invitations" AS "Invitations"
+   WHERE "Invitations"."groupId" = "Group"."id")
+`);
+
+  return await Group.findAll({
+    where: { id: groupId },
+    attributes: [
+      "id",
+      "name",
+      [chatCountSubquery, "chatCount"],
+      [userCountSubquery, "userCount"],
+      [invitationCountSubquery, "invitationCount"],
+    ],
+    include: [
+      {
+        model: Chat,
+        as: "chats",
+        attributes: [],
+        through: {
+          attributes: [],
+        },
+        required: false, // Optional: to include groups even if they have no chats
+      },
+      {
+        model: User,
+        as: "users",
+        attributes: [],
+        through: {
+          attributes: [],
+        },
+        required: false, // Optional: to include groups even if they have no users
+      },
+      {
+        model: Invitation,
+        as: "invitations",
+        attributes: [],
+        required: false, // Optional: to include groups even if they have no invitations
+      },
+    ],
+    group: ["Group.id"],
+  });
+}
+
+const GroupService = { getGroupsByBusiness, getGroupById };
 module.exports = GroupService;
