c59f9ceefcca039485e2aa3bd9ee9fd6f8158efe
mydafzal <momalafzal20@gmail.com>
2024-08-12 14:29:39 +0500

    refactor: remove un-used column from table

--- migrations/20240808061810-create-group-membership.js
+++ migrations/20240808061810-create-group-membership.js
@@ -1,28 +1,25 @@
-'use strict';
+"use strict";
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('GroupMemberships', {
+    await queryInterface.createTable("GroupMemberships", {
       id: {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
-        type: Sequelize.INTEGER
-      },
-      quantity: {
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
       },
       createdAt: {
         allowNull: false,
-        type: Sequelize.DATE
+        type: Sequelize.DATE,
       },
       updatedAt: {
         allowNull: false,
-        type: Sequelize.DATE
-      }
+        type: Sequelize.DATE,
+      },
     });
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('GroupMemberships');
-  }
+    await queryInterface.dropTable("GroupMemberships");
+  },
 };
