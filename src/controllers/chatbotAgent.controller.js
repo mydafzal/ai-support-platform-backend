74e668007b9a8922708c42cd6bba4ce249175caa
mydafzal <momalafzal20@gmail.com>
2024-08-15 15:59:55 +0500

    refactor: remove unnessary text from agent's prompt

--- src/controllers/chatbotAgent.controller.js
+++ src/controllers/chatbotAgent.controller.js
@@ -288,7 +288,7 @@ function createSchedulerAgentPrompt(
 
       1. Initial Inquiry: 
       When a customer indicates a desire to schedule a meeting, prompt them to provide specific details in a step-by-step manner:
-        - First, ask the user to provide the specific month (January to December).
+        - First, ask the user to provide the specific month.
         - Then ask for date of the month.
         - Finally, ask for specific hour in 24-hour format.
         - Don't process until the customer has provided all three.
