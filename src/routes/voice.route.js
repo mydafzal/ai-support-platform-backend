205d7ee1c17347d681398aecc5fe993b3c47d90c
mydafzal <momalafzal20@gmail.com>
2024-08-29 16:34:16 +0500

    feat: implement APIs for retrieving voices and deleting cloned voice

--- src/routes/voice.route.js
+++ src/routes/voice.route.js
@@ -1,14 +1,49 @@
 const router = require("express").Router();
-const { getElevenLabsVoices } = require("../integrations/textToSpeech");
+const {
+  getElevenLabsVoices,
+  deleteVoice,
+} = require("../integrations/textToSpeech");
 
-router.get("/", async (req, res) => {
-  try {
-    const voices = await getElevenLabsVoices();
-    res.status(200).json({ success: true, data: voices });
-  } catch (error) {
-    console.error("Error getting voices", error);
-    res.status(500).json({ success: false, message: "Internal Server Error" });
-  }
-});
+const validateRequest = require("../middleware/request-validation.middleware");
+const asyncHandler = require("../utils/async-handler");
+const ResponseHandler = require("../utils/response-handler");
+
+const { Assistant } = require("../../models");
+
+const { userIdSchema } = require("../validators/user.validator");
+
+router.get(
+  "/",
+  validateRequest(userIdSchema, "query"),
+  asyncHandler(async (req, res) => {
+    const voices = await getElevenLabsVoices(req.query.id);
+    ResponseHandler.success(res, { data: voices });
+  })
+);
+
+router.delete(
+  "/:id",
+  asyncHandler(async (req, res) => {
+    const voiceId = req.params.id;
+
+    const count = await Assistant.count({
+      where: {
+        voiceId,
+      },
+      raw: true,
+    });
+
+    if (count > 0) {
+      return ResponseHandler.error(res, {
+        statusCode: 400,
+        message:
+          "You have one or more companies using this voice. To delete this voice, change the AI voice of companies using this voice.",
+      });
+    }
+
+    await deleteVoice(voiceId);
+    ResponseHandler.success(res, { statusCode: 204 });
+  })
+);
 
 module.exports = router;
